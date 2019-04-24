4f6eed1f524f7fefadcb620e78254498dcd7738d
Roberto Alcover <bacishere@gmail.com>
2019-04-24 15:54:34 +0200

    figuritas

--- codigo/permsVsasintGraficas.py
+++ codigo/permsVsasintGraficas.py
@@ -4,16 +4,16 @@ from os import listdir
 from os.path import isfile,join
 import pylab
 onlfiles = [f for f in listdir("./datos/permutacionesVSasint")]
-o = [s for s in onlfiles if ("RDC" in s)]
+o = [s for s in onlfiles if ("DCOV" in s)]
 #o = onlfiles
 noise = np.linspace(0,3,5)
 for size in [50,100,150,200,500,1000]:
-  actual = [s for s in o if ( "-"+str(size)+"." in s)]
+  actual = [s for s in o if ( "-"+str(size)+"version." in s)]
   figure,ax = plt.subplots(1,5, sharey=True)
-  figure.suptitle("RDC"+str(size))
+  figure.suptitle("DCOV"+str(size))
   for f in actual:
-#    title = f.replace("permutacionesVSasintHSIC-","").replace(str(size),"").replace("-.txt","")
-    title = f.replace("RDC-","").replace("-"+str(size)+".","").replace("txt","")
+#    title = f.replace("permutacionesVSasintDCOV-","").replace(str(size),"").replace("-.txt","")
+    title = f.replace("DCOV-","").replace("-"+str(size)+"version.","").replace("txt","")
     i = int(float(title)*1./0.25)
     print(i)
     solutions = np.loadtxt("./datos/permutacionesVSasint/"+f,delimiter="\t")
@@ -21,4 +21,4 @@ for size in [50,100,150,200,500,1000]:
     ax[i].plot(noise,solutions[1,:],'r',label="real")
     ax[i].set_title(title)
     ax[i].legend(loc='best')
-  plt.show()
+  plt.savefig("figuras/AsimptoticVsReal/DCOV"+str(size)+"version.png")

68b4a012eb867733ed75118b7e03ee73fd3bb864
Roberto Alcover <bacishere@gmail.com>
2019-06-14 13:21:11 +0200

    uhi

--- apuntes/salida/Memoria.lol
+++ apuntes/salida/Memoria.lol
@@ -5,10 +5,12 @@
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
-\contentsline {lstlisting}{\numberline {3.1}Median for x}{30}{lstlisting.3.1}
-\contentsline {lstlisting}{\numberline {3.2}Thead pool example}{30}{lstlisting.3.2}
-\contentsline {lstlisting}{\numberline {3.3}Canonical Correlation Analysis}{33}{lstlisting.3.3}
-\contentsline {lstlisting}{\numberline {3.4}Travis CI yml}{36}{lstlisting.3.4}
+\addvspace {10\p@ }
+\contentsline {lstlisting}{\numberline {4.1}Median for x}{22}{lstlisting.4.1}
+\contentsline {lstlisting}{\numberline {4.2}Thead pool example}{22}{lstlisting.4.2}
+\contentsline {lstlisting}{\numberline {4.3}Canonical Correlation Analysis}{25}{lstlisting.4.3}
+\contentsline {lstlisting}{\numberline {4.4}Travis CI yml}{28}{lstlisting.4.4}
+\addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
--- codigo/erroresDCOV.py
+++ codigo/erroresDCOV.py
@@ -5,18 +5,18 @@ from os import listdir
 from os.path import isfile,join
 import pylab
 onlfiles = [f for f in listdir("./datos/histogramas")]
-o = [s for s in onlfiles if ("DCOV" in s)]
+o = [s for s in onlfiles if ("HSIC" in s)]
 #o = onlfiles
 noise = np.linspace(0,3,5)
 volume_of_experiment = 300
 for size in [50,100,150,200,500,1000]:
   actual = [s for s in o if ( "-"+str(size)+"-" in s)]
   figure,ax = plt.subplots(1,5, sharey=True)
-  figure.suptitle("DCOV"+" Size "+str(size))
+  figure.suptitle("HSIC"+" Size "+str(size))
   asimptotic = np.zeros((5,5))
   for f in actual:
-#    title = f.replace("permutacionesVSasintDCOV-","").replace(str(size),"").replace("-.txt","")
-    title = f.replace("DCOV-","").replace("-"+str(size),"").replace("txt","")
+#    title = f.replace("permutacionesVSasintHSIC-","").replace(str(size),"").replace("-.txt","")
+    title = f.replace("HSIC-","").replace("-"+str(size),"").replace("txt","")
     corelacion,ruido = title.split("-")
     ruido = ruido[:-1]
     ruido_index = int(float(ruido)*1./0.75)
@@ -26,7 +26,7 @@ for size in [50,100,150,200,500,1000]:
     hist = np.loadtxt("./datos/histogramas/"+f,delimiter="\t")
     #asimptotic[cor_index,ruido_index] = np.sum(hist>(chi2.ppf(0.95,df=1)-1))*1./volume_of_experiment
   for i,correlacion in enumerate(np.linspace(0,1,5)): 
-    solutions = np.loadtxt("./datos/permutacionesVSasint/"+"DCOV-"+str(correlacion)+"-"+str(size)+"version.txt",delimiter="\t")
+    solutions = np.loadtxt("./datos/permutacionesVSasint/"+"HSIC-"+str(correlacion)+"-"+str(size)+"version.txt",delimiter="\t")
     ax[i].plot(noise,solutions[1,:],'r',label = "Real")
     ax[i].plot(noise,solutions[0,:],'b',label = "Asymptotic")
     ax[i].set_xlabel("Noise")
@@ -35,4 +35,4 @@ for size in [50,100,150,200,500,1000]:
 
   ax[4].legend(loc='best')
   #plt.show()
-  plt.savefig("figuras/AsimptoticVsReal/DCOV"+str(size)+".png")
+  plt.savefig("figuras/AsimptoticVsReal/HSIC"+str(size)+".png")
